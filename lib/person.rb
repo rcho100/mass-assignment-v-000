84df0b5da66000f76911d9d0ecdb62ffccf63f95
Richard Cho <richardcho100@gmail.com>
2018-05-31 19:33:19 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
+require "pry"
 class Person
-  #your code here
+
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,:weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes_hash)
+    attributes_hash.each {|key, value| self.send("#{key}=", value)}
+  end
 end
