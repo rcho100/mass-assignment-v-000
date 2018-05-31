363248e65d92888f5b7b963be416daddc6bb3010
Richard Cho <richardcho100@gmail.com>
2018-05-31 19:24:24 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,10 @@
+require "pry"
 class Person
 
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,:weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes_hash)
+    binding.pry
     attributes_hash.each {|key, value| self.send (("#{key}="), value)}
   end
 end
